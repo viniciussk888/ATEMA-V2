730fc5ce9646cddb800de7ed26a8a62bcfe963e0
vinicius <vinicius.cross07@gmail.com>
2022-03-20 22:35:36 -0300

    check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Flex } from '@chakra-ui/react';
+import Link from 'next/link';
 import React from 'react';
 import { Header } from '../components/Header';
 import { SideBar } from '../components/Sidebar';
 
 const Blog: React.FC = () => {
     return (
-        <h1> blog <a href='/login'>login</a></h1>
+        <h1> blog <Link href='/login'>login</Link></h1>
     );
 }
 
